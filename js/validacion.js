4bd19dd9cc781c1c448aeea659a4eb63958de79d
AgusMiraballes <agusmiraballesg@gmail.com>
2023-08-10 09:23:05 -0300

    Formulario de registro

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,21 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+let boton = document.getElementById("regBtn");
+let checkbox = document.getElementById("terminos");
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let email = document.getElementById("email");
+let contraseña1 = document.getElementById("password1");
+let contraseña2 = document.getElementById("password2");
+
+
+boton.addEventListener("click", function(){
+if ((contraseña1.value===contraseña2.value) && (checkbox.checked) && ((nombre.value.length && apellido.value.length && email.value.length) >0)
+ && ((contraseña1.value.length) >=6)){
+    showAlertSuccess(); 
+} else {
+    showAlertError();
+}
+})
